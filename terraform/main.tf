7b72723b8d2b6190dc80e3f0f637b2a97a422faf
Sherif127 <Sherifshabanpp00@gmail.com>
2025-05-29 14:29:22 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,30 +2,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Security Group that allows SSH from anywhere
 resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-ssh-sg"
-  description = "Allow SSH from all IPs"
+  name        = "jenkins-sg"
+  description = "Allow SSH & HTTP"
 
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow from all IPs
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# EC2 instance
-resource "aws_instance" "EC2-Jenkins" {
-  ami           = "ami-0953476d60561c955"
+resource "aws_instance" "EC2_Jenkins" {
+  ami           = "ami-0953476d60561c955" # Amazon Linux 2023
   instance_type = "t2.micro"
   key_name      = "mykey"
 
@@ -34,4 +40,8 @@ resource "aws_instance" "EC2-Jenkins" {
   tags = {
     Name = "Jenkins-ec2"
   }
+}
+
+output "aws_instance_ip" {
+  value = aws_instance.EC2_Jenkins.public_ip
 }
